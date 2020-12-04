62e40246d70e5fcba0a4462e3c5e1a9a39580aca
Artem <dr.artem-k@yandex.ru>
2020-12-04 23:02:01 +0400

    выход с акка для диплома

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+/* подключаем пакеты */
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 
+/* импортируем модули */
 const {
   PORT, BD_URL, mongooseConfig,
 } = require('./config');
@@ -13,12 +16,31 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/rateLimit');
 const errorHandler = require('./middlewares/errorHandler');
 
+const corsOptions = {
+  origin: [
+    'http://localhost:8080',
+    'https://www.api.news-card.site/',
+    'https://kravcov7.github.io/news-explorer-frontend/',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: [
+    'Content-Type',
+    'origin',
+    'x-access-token',
+  ],
+  credentials: true,
+};
+
+/* приложение */
 const app = express();
 
 mongoose.connect(BD_URL, mongooseConfig);
 
 app.use(limiter);
 app.use(helmet());
+app.use('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -29,4 +51,6 @@ app.use(errorLogger);
 
 app.use(errorHandler);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
